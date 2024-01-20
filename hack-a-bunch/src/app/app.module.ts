cfc6444ef2cedad846722610c2adc2955aaa36d5
amczuboka <94947740+amczuboka@users.noreply.github.com>
2024-01-20 12:24:40 -0500

    add base components
    Added header and footer, landing page

--- hack-a-bunch/src/app/app.module.ts
+++ hack-a-bunch/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
