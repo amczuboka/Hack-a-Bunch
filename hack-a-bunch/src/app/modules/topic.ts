eaab56c523c9404846634f3984f7c0db3900dd60
amczuboka <94947740+amczuboka@users.noreply.github.com>
2024-01-20 16:52:27 -0500

    Topic class and image
    for topics card

---
+++ hack-a-bunch/src/app/modules/topic.ts
@@ -0,0 +1,13 @@
+export interface Topic {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+export class Topic {
+    title = '';
+    description = '';
+    image = '';
+    link = '';
+}
